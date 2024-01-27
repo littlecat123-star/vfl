e9a1adea9cefdd46f1cb05146b91c347b43177ef
littlecat123-star <1460273162@qq.com>
2024-01-27 12:14:21 +0800

    sixth

--- vfl/src/include.h
+++ vfl/src/include.h
@@ -16,8 +16,8 @@
    Alice's dummy elements will be 3 * 2^xRLEN, and Bob's will be 3 *
    2^xRLEN + 1.
  */
-#include "include-22.h"
-
+#include "include-26.h"
+// #include "include-22.h"
 #define NELEMENTS (1<<LOGN)
 
 #define TWO_TO_TWENTY_BUCKETS 1331692
